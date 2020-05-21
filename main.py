13860667e4a25a789b69a86ea33669bdf7b642ba
Michael J Wolfe <michael.wolfe2@ascension.org>
2020-05-20 22:36:33 -0500

    added Draft 2

---
+++ main.py
@@ -0,0 +1,5 @@
+import pandas as pd
+import tensorflow as tf
+
+raw_data = pd.read_csv('https://raw.githubusercontent.com/mjwolfe91/MSDS_Capstone_JC_AN_MW/master/Data/HAR_FALL-UP_CompleteDataSet.csv')
+raw_data.show()
